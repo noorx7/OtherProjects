8905a526f0edbfba5a7a79c413b7c252f8e94e15
noorx7 <nuruli@mun.ca>
2023-02-04 10:57:36 -0330

    Backend_Complete

--- OpenAI_Codex/client/script.js
+++ OpenAI_Codex/client/script.js
@@ -22,7 +22,7 @@ function typeText(element, text) {
     let index = 0;
     let interval = setInterval(() => {
         if (index < text.length) {
-            element.innerHTML += text.chartAt(index)
+            element.innerHTML += text.charAt(index)
             index++
         } else {
             clearInterval(interval)
@@ -68,6 +68,32 @@ const handleSubmit = async (e) => {
     chatContainer.scrollTop = chatContainer.scrollHeight;
     const messageDiv = document.getElementById(uniqueId)
     loader(messageDiv)
+
+    //fetch data from server 
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = ''
+    if (response.ok) {
+        const data = await response.json()
+        const parsedData = data.bot.trim()
+        typeText(messageDiv, parsedData);
+
+    }
+    else {
+        const err = await response.text();
+        messageDiv.innerHTML = "Something went wrong";
+        alert(err)
+    }
+
 }
 
 form.addEventListener('submit', handleSubmit)
