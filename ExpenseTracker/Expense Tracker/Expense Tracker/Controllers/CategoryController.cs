4592c7109992ff9997e5d188924bbf4ced45699d
noorx7 <nuruli@mun.ca>
2023-05-11 15:37:18 -0300

    Widget_Complete

--- ExpenseTracker/Expense Tracker/Expense Tracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Expense Tracker/Expense Tracker/Controllers/CategoryController.cs
@@ -26,23 +26,7 @@ namespace Expense_Tracker.Controllers
                           Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+      
 
         // GET: Category/AddorEdit
         public IActionResult AddorEdit(int id = 0)
@@ -76,23 +60,7 @@ namespace Expense_Tracker.Controllers
         }
 
 
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+   
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -113,9 +81,5 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
     }
 }
